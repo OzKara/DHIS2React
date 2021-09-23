d436f8573235ab836fbac42bfeef7a79c8636b26
OzKara <oz_247@hotmail.com>
2021-09-23 12:10:41 +0200

    Set up files and create Table component

---
+++ src/Table.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Table(promp) {
+    console.log(promp.apiData);  
+  if (!promp.apiData.results) {
+    // If the API request isn't completed return "loading...""
+    return <p>Loading...</p>;
+  } else {
+    // Write your code here:
+    var tableData = promp.apiData.results;
+    const colm = tableData[0] && Object.keys(tableData[0]);
+    return (
+    <table cellPadding={0} cellSpacing={0}>
+        <thead>
+            <tr>
+                {tableData[0] && colm.map((heading) => <th>{heading}</th>)}
+            </tr>
+        </thead>
+            {tableData.map(row => 
+                <tr>
+                    {colm.map((column) => (
+                        <td>{row[column]}</td>
+                    ))}
+                </tr>
+                )}
+        <tbody>
+            
+        </tbody>
+    </table>
+    );
+  }
+}
+
+export default Table;
